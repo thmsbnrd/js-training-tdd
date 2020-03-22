dc391a988f0f440b0c16e6ed1fdd670484a1b879
Thomas BENARD <thomas0benard@gmail.com>
2020-03-22 14:19:01 +0100

    solved sign.js

--- exercises/sign.js
+++ exercises/sign.js
@@ -11,7 +11,15 @@
  */
 
 // Your code:
-
+function sign(x) {
+  if (x < 0) {
+    return -1;
+  } else if (x > 0) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
 //* Begin of tests
 const assert = require('assert');
 
